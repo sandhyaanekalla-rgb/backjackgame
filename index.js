195516c3f205bdbfd6c0e56f575c01a3e1098844
sandhyaanekalla-rgb <sandhyaanekalla@gmail.com>
2026-04-03 13:44:11 +0530

    I applied some more buttons and also removed some bugs

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let one=(Math.floor(Math.random()*6))+1
-let two=(Math.floor(Math.random()*6))+1
-let blackjack=false
-let isalive=true
-let arr=[one,two];
-let sum=one+two;
+let arr=[];
+let blackjack = false;
+let sum=0;
+let isalive = true;
 let playerdetail={
     name:"Sandhya",
     coins:"$421"
@@ -14,23 +12,27 @@ det.textContent+="coins count :"+playerdetail.coins
 let dis=document.querySelector("#display")
 let card=document.querySelector("#cards")
 let sum1=document.getElementById("sum")
+let msg1=""
 function newcard()
 {
     if(isalive==true&&blackjack==false)
     {
-    let ele=(Math.floor(Math.random()*6))+1
+    let ele=(Math.floor(Math.random()*10))+1
     arr.push(ele)
     sum+=ele
     startagain()
     }
     else if(blackjack==false){
-        msg="sorry u can't draw new card anymore🥲😭"
+        msg1="sorry u can't draw new card anymore🥲😭"
+        dis.textContent=msg1
     }
     else{
-        msg="hey "+playerdetail.name+" you already won🥳 ur chompion you know  with "+playerdetail.coins+" coins at you"
+        msg1="hey "+playerdetail.name+" you already won🥳 ur chompion you know  with "+playerdetail.coins+" coins at you .."
+        dis.textContent=msg1
     }
-    dis.textContent=msg
+    
 }
+
 function startagain()
 {
     card.textContent="CARDS ARE : "
@@ -57,8 +59,42 @@ function startagain()
        
     }
     dis.textContent=msg
-}
+}let s=0;
 function start()
 {
-    startagain()
+    if(s==0){
+    arr=[];
+    blackjack = false;
+    isalive = true;
+
+    arr.push((Math.floor(Math.random()*10)+1))
+    arr.push((Math.floor(Math.random()*10)+1))
+    sum=0
+   for(let i=0;i<arr.length;i++)
+   {
+    sum+=arr[i];
+   }s=1;
+   startagain()
+}
+else{
+        let msg1="already started!! u can restart"
+        dis.textContent=msg1;
+}
+}
+function re()
+{
+    
+    arr=[];
+    blackjack = false;
+    isalive = true;
+
+    arr.push((Math.floor(Math.random()*10)+1))
+    arr.push((Math.floor(Math.random()*10)+1))
+    sum=0
+   for(let i=0;i<arr.length;i++)
+   {
+    sum+=arr[i];
+   }s=1;
+   startagain()
+    
 }
